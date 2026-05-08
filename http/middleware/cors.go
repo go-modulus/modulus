4c46ce1d506f069369d2ac33fc44d1d8766f1caf
Oleksii Holubivskyi <oleksii.holubivskyi@softconstruct.com>
2026-05-08 16:22:08 +0300

    cors configurable AllowedMethods and AllowedHeaders with environment variables

--- http/middleware/cors.go
+++ http/middleware/cors.go
@@ -8,7 +8,9 @@ import (
 )
 
 type CorsConfig struct {
-	Host string `env:"CORS_HOST, default=^https?://(localhost|127.0.0.1)(:[0-9]+)?$"`
+	Host           string   `env:"CORS_HOST, default=^https?://(localhost|127.0.0.1)(:[0-9]+)?$"`
+	AllowedMethods []string `env:"CORS_ALLOWED_METHODS"`
+	AllowedHeaders []string `env:"CORS_ALLOWED_HEADERS"`
 }
 
 func NewCors(config CorsConfig) *cors.Cors {
@@ -18,35 +20,43 @@ func NewCors(config CorsConfig) *cors.Cors {
 	}
 	corsRegexp := regexp.MustCompile(host)
 
+	allowedMethods := config.AllowedMethods
+	if len(allowedMethods) == 0 {
+		allowedMethods = []string{
+			http.MethodGet,
+			http.MethodHead,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodOptions,
+			http.MethodDelete,
+		}
+	}
+
+	allowedHeaders := config.AllowedHeaders
+	if len(allowedHeaders) == 0 {
+		allowedHeaders = []string{
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Language",
+			"Authorization",
+			"Content-Type",
+			"Content-Length",
+			"Cache-Control",
+			"Connection",
+			"Pragma",
+			"Cookie",
+			"Access-Control-Allow-Origin",
+			"User-Agent",
+			"Last-Event-Id",
+		}
+	}
+
 	return cors.New(
 		cors.Options{
-			AllowOriginFunc: func(origin string) bool {
-				return corsRegexp.Match([]byte(origin))
-			},
-			AllowedMethods: []string{
-				http.MethodGet,
-				http.MethodHead,
-				http.MethodPost,
-				http.MethodPut,
-				http.MethodPatch,
-				http.MethodOptions,
-				http.MethodDelete,
-			},
-			AllowedHeaders: []string{
-				"Accept",
-				"Accept-Encoding",
-				"Accept-Language",
-				"Authorization",
-				"Content-Type",
-				"Content-Length",
-				"Cache-Control",
-				"Connection",
-				"Pragma",
-				"Cookie",
-				"Access-Control-Allow-Origin",
-				"User-Agent",
-				"Last-Event-Id",
-			},
+			AllowOriginFunc:    func(origin string) bool { return corsRegexp.Match([]byte(origin)) },
+			AllowedMethods:     allowedMethods,
+			AllowedHeaders:     allowedHeaders,
 			ExposedHeaders:     nil,
 			MaxAge:             3600,
 			AllowCredentials:   true,
